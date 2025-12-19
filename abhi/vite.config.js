bfd2d8ec82c8e2f9aa3967b7e242dc1685be4842
Abhishek pal <ap5012759@gmail.com>
2025-12-19 16:14:46 +0530

    Update vite.config.js

--- abhi/vite.config.js
+++ abhi/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://medical-plants-backend-4ymm.vercel.app',
         changeOrigin: true,
         secure: false,
       },
